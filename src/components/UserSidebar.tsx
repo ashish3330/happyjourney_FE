8cf1ce4bdb85bd9c94ac1bdf1e93e337d4ee1b4e
Abhishek1269 <abhishekgupta8920119493@gmail.com>
2025-07-08 22:16:39 +0530

    logo issure resolved

--- src/components/UserSidebar.tsx
+++ src/components/UserSidebar.tsx
@@ -45,7 +45,7 @@ const UserSidebar = forwardRef<HTMLDivElement, UserSidebarProps>(({ collapsed, s
           </div>
         )}
       </div>
-      <nav className={`mt-4 ${collapsed ? "hidden md:block" : "block"}`}>
+      <nav className={`mt-10 ${collapsed ? "hidden md:block" : "block"}`}>
         {userMenuItems.map((item, index) => {
           const isActive = location.pathname === item.path;
           return (
