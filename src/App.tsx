b522fde6c64c14ac8628391a04881320a04937dd
ashish <chaurasiyaashish007@gmaill.com>
2025-07-13 20:45:27 +0530

    Footer changed

--- src/App.tsx
+++ src/App.tsx
@@ -31,14 +31,14 @@ import BulkOrdersDashboard from "./pages/BulkOrderDashboard";
 import ComplaintsDashboard from "./pages/ComplaintsDashboard";
 import CallbacksDashboard from "./pages/CallbacksDashboard";
 import OrdersExportDashboard from "./pages/OrdersExportDashboard";
-import { RelswadFooter } from "./components/FooterConfigs";
+import { HappyJourneyFooter } from "./components/FooterConfigs";
 import UserPasswordlessLogin from "./pages/UserPasswordlessLogin";
 import ShippingPolicy from "./pages/ShippingPolicy";
 import ContactForm from "./pages/ContactForm";
 
 const FooterWrapper = () => {
   const { accessToken } = useAuth();
-  return accessToken ? <RelswadFooter /> : null;
+  return accessToken ? <HappyJourneyFooter /> : null;
 };
 
 const App = () => {
